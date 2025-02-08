aede4d515aac9d3d6f74423f0512def13e0eb960
Shyrine Salvador <84778328+shyrxnx@users.noreply.github.com>
2025-02-08 21:25:04 +0800

    refactor: update to include BrowserRouter and Router for routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import Router from "./routes/Router";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <BrowserRouter>
+            <Router />
+        </BrowserRouter>
+    </StrictMode>
+);
