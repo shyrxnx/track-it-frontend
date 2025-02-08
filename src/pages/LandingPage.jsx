1245a1c51b6c687b48f1aa341d984ae838b7ef97
Shyrine Salvador <84778328+shyrxnx@users.noreply.github.com>
2025-02-08 21:25:58 +0800

    feat: add initial LandingPage

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+import "../index.css";
+import LogoAndTitle from "../components/LogoAndTitle";
+
+function LandingPage() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen min-w-screen">
+            {/* Logo and Title */}
+            <LogoAndTitle size="large" />
+
+            {/* Description */}
+            <p className="text-lg max-w-3xl text-center">
+                Track-It is a web-based habit tracker that helps users build and maintain daily routines with
+                insightful analytics and progress tracking.
+            </p>
+
+            {/* Button */}
+            <div className="mt-8">
+                <Link to="/dashboard">
+                    <button className="transition duration-300">
+                        Start Tracking
+                    </button>
+                </Link>
+            </div>
+        </div>
+    );
+}
+
+export default LandingPage;
